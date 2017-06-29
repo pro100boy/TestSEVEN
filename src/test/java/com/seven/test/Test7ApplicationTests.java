d521482ef7e2e40d4baee6971d12121d9b9a0d77
Pavel <gpg@ua.fm>
2017-06-29 17:44:54 +0300

    tests init

--- src/test/java/com/seven/test/Test7ApplicationTests.java
+++ src/test/java/com/seven/test/Test7ApplicationTests.java
@@ -1,16 +1,24 @@
 package com.seven.test;
 
+import com.seven.test.controller.LoginController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Test7ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private LoginController loginController;
+
+    @Test
+    public void contextLoads() {
+        assertThat(loginController).isNotNull();
+    }
 
 }
