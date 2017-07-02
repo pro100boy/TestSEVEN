6625d3853a6bd82e868e7e6e66964d66c5b69702
Pavel <gpg@ua.fm>
2017-07-02 15:57:39 +0300

    lombok

--- src/main/java/com/seven/test/model/Role.java
+++ src/main/java/com/seven/test/model/Role.java
@@ -1,22 +1,18 @@
 package com.seven.test.model;
 
-import javax.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "role")
 public class Role extends BaseEntity{
 
     @Column(name = "role")
     private String role;
-
-    public Role() {
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
